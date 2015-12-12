cf6a949b89f2c13ceccbc12dda2aa7026db029a8
Zachary Lee <w.zachary.lee@gmail.com>
2015-12-12 14:24:40 -0800

    fixed oauth on refresh

--- electron/init.js
+++ electron/init.js
@@ -16,7 +16,14 @@ function getGithubTokenFromLocalStorage(webcontents, done){
     if(err || cookies.length === 0){
       done(null, err)
     } else {
-      done(null);
+      var githubCode = cookies.reduce(function(prevVal, currentVal) {
+        if (!prevVal && currentVal.name === 'github') {
+          return currentVal.value
+        } else {
+          return prevVal
+        }
+      }, null)
+      done(githubCode)
     }
   })
 }
@@ -25,6 +32,7 @@ function directToGithub (mainWindow, done){
   mainWindow.loadURL(authUrl);
   mainWindow.show();
   mainWindow.webContents.on('will-navigate', function (event, url) {
+    console.log('url', url)
     if (url.indexOf(callbackUrl) !== -1) {
       done(url.split('=')[1])
     } else {
@@ -33,17 +41,17 @@ function directToGithub (mainWindow, done){
   })
 }
 
-function setTokenInLocalStorage(webcontents, token, done){
+function setTokenInLocalStorage(webcontents, code, done){
   var options = {
     name: 'github',
-    value : token,
+    value : code,
     url : 'http://www.github.com'
   }
   webcontents.session.cookies.set(options, function(err){
     if(err){
       done.fail(err)
     } else {
-      done(token)
+      done(code)
     }
   })
 }
@@ -57,9 +65,38 @@ function clearLocalStorage(webContents){
   })
 }
 
-
-module.exports = {
-  getGithubTokenFromLocalStorage : getGithubTokenFromLocalStorage,
-  directToGithub : directToGithub,
-  setTokenInLocalStorage : setTokenInLocalStorage
+function init (mainWindow) {
+  //get code
+    //get from local storage
+  //if response=good
+    //load page
+  //if response =not good
+    //get code from github
+    //load page
+  ASQ(function(done){
+    getGithubTokenFromLocalStorage(mainWindow.webContents, done)
+  })
+  .then(function(done, data){
+    console.log('data from git', data);
+    if(data){
+      console.log('got it');
+      mainWindow.loadURL(`file://${__dirname}/public/index.html`);
+    } else {
+      console.log('ayyo')
+      directToGithub(mainWindow, done)
+    }
+  })  
+  .then(function(done, code){
+    console.log('code ', code);  
+    setTokenInLocalStorage(mainWindow.webContents, code, done)
+  })
+  .then(function(done, code){
+    mainWindow.loadURL(`file://${__dirname}/public/index.html`);
+  })
+  .or(function(err){
+    console.log('err ', err)
+  })
 }
+
+
+module.exports = {init: init}
